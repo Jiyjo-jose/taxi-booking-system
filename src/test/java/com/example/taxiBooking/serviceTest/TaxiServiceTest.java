15d2eccebb2c5dcd7133eaed6142dbe793802395
Jiyjo-jose <jiyjo.jose@edstem.com>
2024-02-02 16:34:31 +0530

    taxi service test

--- src/test/java/com/example/taxiBooking/serviceTest/TaxiServiceTest.java
+++ src/test/java/com/example/taxiBooking/serviceTest/TaxiServiceTest.java
@@ -1,4 +1,46 @@
 package com.example.taxiBooking.serviceTest;
 
+import com.example.taxiBooking.contract.request.SignUpRequest;
+import com.example.taxiBooking.contract.request.TaxiRequest;
+import com.example.taxiBooking.contract.response.SignUpResponse;
+import com.example.taxiBooking.contract.response.TaxiResponse;
+import com.example.taxiBooking.model.Taxi;
+import com.example.taxiBooking.model.User;
+import com.example.taxiBooking.repository.TaxiRepository;
+import com.example.taxiBooking.service.TaxiService;
+import com.example.taxiBooking.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
 public class TaxiServiceTest {
+
+    private TaxiRepository taxiRepository;
+    private TaxiService taxiService;
+    private  ModelMapper modelMapper;
+    @BeforeEach
+    public void init(){
+        MockitoAnnotations.openMocks(this);
+        taxiRepository = Mockito.mock(TaxiRepository.class);
+        modelMapper=new ModelMapper();
+        taxiService=new TaxiService(modelMapper,taxiRepository);
+    }
+
+    @Test
+    void testAddTaxi(){
+        TaxiRequest taxiRequest = new TaxiRequest(null,null,null);
+        Taxi savedTaxi= new Taxi(null,null,null,null,new ArrayList<>());
+        TaxiResponse expectedResponse= modelMapper.map(savedTaxi, TaxiResponse.class);
+        when(taxiRepository.save(any())).thenReturn(savedTaxi);
+        TaxiResponse actualResponse=taxiService.addTaxi(taxiRequest);
+        assertEquals(expectedResponse,actualResponse);
+    }
 }
